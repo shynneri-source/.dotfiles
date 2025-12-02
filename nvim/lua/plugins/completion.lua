c6562b9ac44e8d7c866f7b94e9436656a9f3bca2
shynneri-source <nguyentrongphuoc.ai@gmail.com>
2025-12-02 09:54:54 +0700

    add some logic completion

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,19 +1,23 @@
 return {
     "hrsh7th/nvim-cmp",
     dependencies = {
-        "hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
-        "hrsh7th/cmp-buffer", -- Buffer source
-        "hrsh7th/cmp-path", -- Path source
-        "L3MON4D3/LuaSnip", -- Snippet engine
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
     },
     config = function()
         local cmp = require("cmp")
+        local luasnip = require("luasnip")
         cmp.setup({
             snippet = {
                 expand = function(args)
                     require("luasnip").lsp_expand(args.body)
                 end,
+            },
+            completion = {
+                completeopt = 'menu,menuone,noselect'
             },
             mapping = cmp.mapping.preset.insert({
                 ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -21,13 +25,32 @@ return {
                 ['<C-Space>'] = cmp.mapping.complete(),
 
                 ['<CR>'] = cmp.mapping(function(fallback)
-                    fallback()
+                    if cmp.visible() and cmp.get_selected_entry() then
+                        cmp.confirm({ select = false })
+                    else
+                        fallback()
+                    end
                 end, { "i", "s" }),
 
-                ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+                ['<Tab>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
 
-                ['<C-n>'] = cmp.mapping.select_next_item(),
-                ['<C-p>'] = cmp.mapping.select_prev_item(),
+                ['<S-Tab>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
             }),
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
