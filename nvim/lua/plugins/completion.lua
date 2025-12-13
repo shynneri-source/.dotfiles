7a1810f215930d7ef48511e500f3a94f6cee6ee6
shynneri-source <nguyentrongphuoc.ai@gmail.com>
2025-12-13 10:40:29 +0700

    add something

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -31,27 +31,26 @@ return {
 
       formatting = {
         format = lspkind.cmp_format({
-          mode = "symbol_text", -- show icon and text
-          maxwidth = 50,        -- prevent the popup from being too wide
+          mode = "symbol_text",
+          maxwidth = 50,
           ellipsis_char = "...",
-          show_labelDetails = true, 
+          show_labelDetails = true,
         }),
       },
 
       -- Mappings
       mapping = cmp.mapping.preset.insert({
-        ["<C-k>"] = cmp.mapping.select_prev_item(), -- Navigate up
-        ["<C-j>"] = cmp.mapping.select_next_item(), -- Navigate down
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(), -- Trigger completion manually
+        ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
-        
-        ["<CR>"] = cmp.mapping.confirm({ select = true }), 
 
-        ["<Tab>"] = cmp.mapping(function(fallback)
+       ["<CR>"] = cmp.mapping.confirm({ select = true }),
+       ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
-            cmp.select_next_item()
+           cmp.select_next_item() 
           elseif luasnip.expand_or_jumpable() then
             luasnip.expand_or_jump()
           else
@@ -71,11 +70,11 @@ return {
       }),
 
       sources = cmp.config.sources({
-        { name = "nvim_lsp" }, -- LSP suggestions
-        { name = "luasnip" },  -- Snippets
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
       }, {
-        { name = "buffer" },   -- Text from current buffer
-        { name = "path" },     -- File paths
+        { name = "buffer" },
+        { name = "path" },
       }),
     })
   end,
